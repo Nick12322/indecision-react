47e30803e385612d102858df7a0359fae2246296
Nick <nickwoodward5@gmail.com>
2019-01-29 09:08:28 -0800

    Section 3, lecture 10 complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,40 @@
 console.log("App.js is running");
 
 var template = React.createElement(
-  "h1",
-  null,
-  "App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Test"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is info"
+    )
+);
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Nick Woodward"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 23"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: San Diego, CA"
+    )
 );
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 console.log("App.js is running")
 
-var template = <h1>App</h1>;
+var template = ( 
+    <div>
+        <h1>Test</h1>
+        <p>This is info</p>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Nick Woodward</h1>
+        <p>Age: 23</p>
+        <p>Location: San Diego, CA</p>
+    </div>
+);
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
