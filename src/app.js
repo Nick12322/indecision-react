80effc38f877427ccc479d0219f263c468cf2cf7
Nick <nickwoodward5@gmail.com>
2019-01-31 11:18:41 -0800

    Section 4, lecture 27 complete

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        );
+    };
+};
+
 class Header extends React.Component {
     render() {
         return (
@@ -24,6 +37,17 @@ class Options extends React.Component {
         return (
             <div>
                 <p>Options Component Here</p>
+                <Option />
+            </div>
+        );
+    };
+};
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Option Component Here</p>
             </div>
         );
     };
@@ -39,13 +63,4 @@ class AddOption extends React.Component {
     };
 };
 
-const jsx = (
-    <div>
-        <Header />
-        <Action />
-        <Options />
-        <AddOption />
-    </div>
-);
-
-ReactDOM.render(jsx, document.getElementById("app"));
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
