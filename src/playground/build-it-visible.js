b1d676e297e20e56246a33ad6c57c44e1835311b
Nick <nickwoodward5@gmail.com>
2019-02-01 20:10:15 -0800

    section 4, lecture 35 complete

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,47 +1,78 @@
-const app = {
-    deatailDisplay: false,
-};
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleVisbilityChange = this.handleVisbilityChange.bind(this);
+        this.state = {
+            visbility: false,
+        };
+    };
 
+    handleVisbilityChange() {
+        this.setState((prevState) => {
+            return {
+                visbility: !prevState.visbility
+            };
+        });
+    };
 
-const toggleDisplay = () => {
-    console.log("display test")
-    if (app.detailDisplay) {
-        app.detailDisplay = false;
-        console.log(app.detailDisplay);
-        render();
-    } else {
-        app.detailDisplay = true;
-        console.log(app.detailDisplay);
-        render();
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleVisbilityChange}>{this.state.visbility ? "Click to hide details" : "Click to show details"}</button>
+                {this.state.visbility && <p>Some details</p>}
+            </div>
+        );
     };
 };
 
+ReactDOM.render(<VisibilityToggle />, document.getElementById("app"));
 
 
-const appRoot = document.getElementById("app");
 
-const render = () => {
-
-    let details;
-    let buttonText;
-    if (app.detailDisplay) {
-        details = <p>Here's some details!</p>
-        buttonText = "Details Shown!"
-    } else {
-        details
-        buttonText = "Click to see details!"
-    };
-
-    const template = (
-        <div>
-            <h1>Visbility Toggle</h1>
-            <button name="toggle" onClick={toggleDisplay}>{buttonText}</button>
-            {details}
-        </div>
-    );
-    
-    ReactDOM.render(template, appRoot);
-};
+// const app = {
+//     deatailDisplay: false,
+// };
 
 
-render();
+// const toggleDisplay = () => {
+//     console.log("display test")
+//     if (app.detailDisplay) {
+//         app.detailDisplay = false;
+//         console.log(app.detailDisplay);
+//         render();
+//     } else {
+//         app.detailDisplay = true;
+//         console.log(app.detailDisplay);
+//         render();
+//     };
+// };
+
+
+
+// const appRoot = document.getElementById("app");
+
+// const render = () => {
+
+//     let details;
+//     let buttonText;
+//     if (app.detailDisplay) {
+//         details = <p>Here's some details!</p>
+//         buttonText = "Details Shown!"
+//     } else {
+//         details
+//         buttonText = "Click to see details!"
+//     };
+
+//     const template = (
+//         <div>
+//             <h1>Visbility Toggle</h1>
+//             <button name="toggle" onClick={toggleDisplay}>{buttonText}</button>
+//             {details}
+//         </div>
+//     );
+
+//     ReactDOM.render(template, appRoot);
+// };
+
+
+// render();
