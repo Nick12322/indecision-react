3bd1b68ac21772e2c2870d2b322f3da9ed493f9f
Nick <nickwoodward5@gmail.com>
2019-02-05 14:51:48 -0800

    section 6, lecture 51 complete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("App.js is running!!");
+// import "./utils.js";
+// import { square, add } from "./utils.js";
+
+// console.log("App.js is running!!");
+// console.log(square(4));
+// console.log(add(2, 8));
+
+import { isAdult, canDrink } from "./person.js"
+
+console.log(isAdult(5));
+console.log(canDrink(5));
