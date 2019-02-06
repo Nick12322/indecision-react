14698fe3a20ff2fab547af515b4a5a82eb73c48f
Nick <nickwoodward5@gmail.com>
2019-02-05 20:28:48 -0800

    section 6, lecture 53 complete

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-// import "./utils.js";
-// import subtract, { square, add  } from "./utils.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log("App.js is running!!");
-// console.log(square(4));
-// console.log(add(2, 8));
-// console.log(subtract(20, 10));
-// import { isAdult, canDrink } from "./person.js"
-
-console.log(isAdult(5));
-console.log(canDrink(5));
-console.log(isSenior(62));
-
-import isSenior, {isAdult, canDrink} from "./person";
+const template = React.createElement('p', {}, "testing");
+ReactDOM.render(template, document.getElementById('app'));
